6e43192c2b83b063b35c891903e3e8ac595fa569
Ashish Masih <masih.ashish@gmail.com>
2023-05-09 12:44:14 -0500

    minor edits made main.css always load

--- pages/head.php
+++ pages/head.php
@@ -5,8 +5,9 @@ header("Expires: 0"); // Proxies.
 ?>
 
 <?php
-echo "Page CSS detected in head ".$page_css;
+echo "Page CSS detected in head " . $page_css;
 ?>
+
 <head>
     <!-- Google tag (gtag.js) -->
     <script async src="https://www.googletagmanager.com/gtag/js?id=G-JNPG6CTKFN"></script>
@@ -26,16 +27,13 @@ echo "Page CSS detected in head ".$page_css;
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width">
     <title>Daily EC data from towers.</title>
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/css/bootstrap.min.css"
-     rel="stylesheet" integrity="sha384-KK94CHFLLe+nY2dmCWGMq91rCGa5gtU4mk92HdvYe+M/SXH301p5ILy+dN9+nJOZ"
-      crossorigin="anonymous">
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/js/bootstrap.bundle.min.js"
-     integrity="sha384-ENjdO4Dr2bkBIFxQpeoTz1HIcje39Wm4jDKdf19U8gI4ddQ3GYNS7NTKfAdVQSZe"
-      crossorigin="anonymous"></script>
-      <?php
-    echo '<link rel="stylesheet" type="text/css" href='.$page_css.'>';
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-KK94CHFLLe+nY2dmCWGMq91rCGa5gtU4mk92HdvYe+M/SXH301p5ILy+dN9+nJOZ" crossorigin="anonymous">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ENjdO4Dr2bkBIFxQpeoTz1HIcje39Wm4jDKdf19U8gI4ddQ3GYNS7NTKfAdVQSZe" crossorigin="anonymous"></script>
+    <?php if (!$page_css === "./CSS/Main.css") {
+        echo '<link rel="stylesheet" type="text/css" href=' . $page_css . '>';
+    }
     ?>
-    <!-- <link rel="stylesheet" type="text/css" href="./CSS/Main.css"> -->
+    <link rel="stylesheet" type="text/css" href="./CSS/Main.css">
     <link rel="stylesheet" type="text/css" href="./pages/NavBar.css">
 
     <link rel="stylesheet" type="text/css" href="./CSS/stations.css">
