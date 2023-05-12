9ffa9f2003e37f4211dd10ef4b3117c1184d6caf
Ashish Masih <masih.ashish@gmail.com>
2023-05-12 07:04:05 -0500

    sync

--- pages/Footer.php
+++ pages/Footer.php
@@ -3,7 +3,7 @@
         <div class="row">
             <div class="col-4"><a href="https://parallel41-dev.web.app/#/documentation" target="_blank">Credits</a></div>
             <div class="col-4"><a href="https://hprcc.unl.edu/" target="_blank">Data Sources</a></div>
-            <div class="col-4"><a href="https://github.com/ashish-codebase/ashish-codebase.github.io" target="_blank">Source Code @ Github</a></div>
+            <div class="col-4"><a href="https://github.com/ashish-codebase/CSCE-Site" target="_blank">Source Code @ Github</a></div>
         </div>
     </div>
 </footer>
