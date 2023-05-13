872ceadc35dd87447482dc4c150b55097dae5995
Ashish Masih <masih.ashish@gmail.com>
2023-05-13 14:22:36 -0500

    New Commits

--- pages/head.php
+++ pages/head.php
@@ -27,8 +27,7 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width">
     <title>Daily EC data from towers.</title>
-    <link href="./CSS/bootstrap.min.css"
-     rel="stylesheet" integrity="sha384-KK94CHFLLe+nY2dmCWGMq91rCGa5gtU4mk92HdvYe+M/SXH301p5ILy+dN9+nJOZ" crossorigin="anonymous">
+    <link href="./CSS/bootstrap.min.css" rel="stylesheet" integrity="sha384-KK94CHFLLe+nY2dmCWGMq91rCGa5gtU4mk92HdvYe+M/SXH301p5ILy+dN9+nJOZ" crossorigin="anonymous">
     <script src="./js/bootstrap.bundle.min.js" integrity="sha384-ENjdO4Dr2bkBIFxQpeoTz1HIcje39Wm4jDKdf19U8gI4ddQ3GYNS7NTKfAdVQSZe" crossorigin="anonymous"></script>
     <?php if ($page_css != "./CSS/Main.css") {
         echo '<link rel="stylesheet" type="text/css" href=' . $page_css . '>';
